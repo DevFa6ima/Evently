9c44e51b10525f2cfc35d7d043ad09f17bccdf30
Fatima Alkhomayes <ftooom655@gmail.com>
2024-12-08 21:05:36 +0300

    add to app

---
+++ EventlyApp/app/build/generated/source/buildConfig/androidTest/debug/com/evently/eventlyapp/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.evently.eventlyapp.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.evently.eventlyapp.test";
+  public static final String BUILD_TYPE = "debug";
+}
